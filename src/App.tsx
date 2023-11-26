b5a7e7aa678b1c6151efb2102b1355b60e72c141
TEDKORDEV <ted.kordev@gmail.com>
2023-11-26 15:48:10 +0900

    setting done

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@
  * @format
  */
 
-import React from 'react'
-import type {PropsWithChildren} from 'react'
+import React from 'react';
+import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -15,7 +15,7 @@ import {
   Text,
   useColorScheme,
   View,
-} from 'react-native'
+} from 'react-native';
 
 import {
   Colors,
@@ -23,14 +23,14 @@ import {
   Header,
   LearnMoreLinks,
   ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen'
+} from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
 function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark'
+  const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
       <Text
@@ -52,15 +52,15 @@ function Section({children, title}: SectionProps): JSX.Element {
         {children}
       </Text>
     </View>
-  )
+  );
 }
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark'
+  const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  }
+  };
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -79,6 +79,7 @@ function App(): JSX.Element {
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
+            
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
@@ -93,7 +94,7 @@ function App(): JSX.Element {
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -113,6 +114,6 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
-})
+});
 
-export default App
+export default App;
